7e96b349ee3a2aece26c1c879ada0bb938c1b793
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-05-11 01:19:03 +0100

    reposition of the meme image

--- src/section2/meme.js
+++ src/section2/meme.js
@@ -35,7 +35,7 @@ function Meme() {
                     Get a new meme image 🖼
                      </button>
             </div>
-            <img src={memeImage} alt="memeImage" />
+            <img src={memeImage} className="meme-image" alt="memeImage" />
         </main>
 
 
