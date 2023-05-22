a182d2ba6ea916f686513186fb5f77ba5d90c1f6
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-05-22 22:56:19 +0100

    React parent useState property working

--- src/section2/box.js
+++ src/section2/box.js
@@ -10,22 +10,27 @@ export default function Box(props) {
     
     
     function toggle(id) {
-         setSquares(prevSquares => {
-          const newSquares = []
-          for(let i = 0; i  < prevSquares.length; i++) {
-              const currentSquare = prevSquares[i]
-              if(currentSquare.id === id) {
-                const updatedSquare = {
-                  ...currentSquare,
-                  on: !currentSquare.on
-                }
-                newSquares.push(updatedSquare)
-              }else {
-                newSquares.push(currentSquare)
-              }
-          }
-          return newSquares 
-         } )
+      setSquares(prevSquare => {
+        return prevSquare.map((square) => {
+         return square.id === id ? {...square, on: !square.on} : square
+        })
+      })
+        //  setSquares(prevSquares => {
+        //   const newSquares = []
+        //   for(let i = 0; i  < prevSquares.length; i++) {
+        //       const currentSquare = prevSquares[i]
+        //       if(currentSquare.id === id) {
+        //         const updatedSquare = {
+        //           ...currentSquare,
+        //           on: !currentSquare.on
+        //         }
+        //         newSquares.push(updatedSquare)
+        //       }else {
+        //         newSquares.push(currentSquare)
+        //       }
+        //   }
+        //   return newSquares 
+        //  } )
          
       }
       const squareElement = squares.map(square => (
