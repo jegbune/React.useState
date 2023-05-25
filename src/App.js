e6c12f217c05ffb11b519a1ab338f9036c8b5fa2
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-05-25 09:22:08 +0100

    Restart React lesson

--- src/App.js
+++ src/App.js
@@ -1,86 +1,24 @@
 import React from "react"
-// import logo from './logo.svg';
-// import './App.css'
-// import Header from "./section/header.js";
-// import Trip from "./section/trip.js";
-// import data from "./section/data.js";
-// import "./section2/style2.css";
-// import Header from "./section2/header1.js";
-// import Meme from "./section2/meme.js";
-// import memesData from "./section/memesData.js";
-// import "./section2/index2.css";
-// import Counter from "./section2/index3.js";
-// import Exercise from "./section2/exercise.js";
-// import "./section2/style3.css";
-// import Index from "./section2/index.js";
-// import Index2 from "./section2/index2.js";
-// import Card from "./section2/Card.js"
-// import "./section2/card.css"
-// import Header from "./section2/header.js"
-// import Body from "./section2/Body.js"
-// import "./section2/body.css"
-import "./section2/boxes.css"
-import Box from "./section2/box";
+
 
 function App() {
-  // const content = data.map(item =>{
-  //   return (
-  //     <Trip
-  //       {...item}
-  //     />
-  //   )
-  // })
-
-
-  
-  // const [user, setUser] = React.useState("Bobs")
-  
-  return (
-    // <div className="container">
-    // <Exercise />
-    // </div>
-    <div>
-     {/* <Index2 />
-     <Index /> 
-     <Card /> */}
-      {/* <Counter />  */}
-      {/*  <Header />
-      <Meme /> */}
-
-      {/* <Header
-      name = {user}  />
-      <Body
-      name = {user}  /> */}
-        <Box
-        darkMode = {true}
-        />
-
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
-    // <div>
-      // <Header />
-    //   <section>
-    //     {content}
-
-    //   </section>
-    // </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/section2/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header(props) {
-    return (
-        <header>
-            <p>Current user: {props.name}</p>
-        </header>
-    )
-}
