5d0cf9162b8efa1c292d5719abc5e02173f2e792
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-05-31 01:40:20 +0100

    React Props JSX

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,15 +1,20 @@
 import React from "react"
-const Greet = () =>{
-return React.createElement (
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        {id:"Hello", className: "dummyClass"},
-        "Hello Jeremiah Egbune"
-    )
-    // {id: "Hello", className: "dummyClass"},
-    // "Hello Jeremiah"
-/* <h1>Hello Jeremiah</h1> */
-)}
+// const Greet = () =>{
+// return React.createElement (
+//     "div",
+//     null,
+//     React.createElement(
+//         "h1",
+//         {id:"Hello", className: "dummyClass"},
+//         "Hello  Jeremiah"
+//     )
+//     // {id: "Hello", className: "dummyClass"},
+//     // "Hello Jeremiah"
+// /* <h1>Hello Jeremiah</h1> */
+// )}
+
+const Greet = props => {
+    
+return <h1>Hello {props.name}</h1>
+}
 export default Greet
