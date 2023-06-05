3753ba62d8bc583f736db7fa1cd0eceba8d67b26
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-05 22:03:38 +0100

    React class component setSet

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,9 +3,37 @@ import React, { Component } from "react";
 class Counter extends Component {
 
   constructor() {
+    super() 
     
+    this.state = {
+      count: 0
+    }
+
   }
+  Increment(){
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  Decrement(){
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Count - {this.state.count}</h1>
+        <button onClick={() => this.Increment()}>Increment</button>
+        <button onClick={() => this.Decrement()}>Decrement</button>
+      </div>
+    )
+  }
+
 }
+
 
 
 
