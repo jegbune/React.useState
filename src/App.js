da833d97872a6418fd74a1e3575771870aaaa2b1
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-13 04:05:26 +0100

    React jS module css

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import Stylesheet from "./components/stylesheet";
 import Inline from "./inline";
+import "./appStyles.css"
+import styles from "./appStyles.module.css"
+
+
 class App extends Component {
   render()  {
     return (
       <div className="App">
+        <h1 className= 'error'>Error</h1>
+        <h1 className= {styles.sucess}>Sucess</h1>
         {/* <Inline /> */}
-        <Stylesheet primary = {true} />
+        {/* <Stylesheet primary = {true} /> */}
         {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
