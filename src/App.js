d3a943fd1d8ae9bb36c2fbeef637978cbaeec105
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-15 04:14:33 +0100

    React js Form Component

--- src/App.js
+++ src/App.js
@@ -14,14 +14,16 @@ import Stylesheet from "./components/stylesheet";
 import Inline from "./inline";
 import "./appStyles.css"
 import styles from "./appStyles.module.css"
+import Form from "./Form";
 
 
 class App extends Component {
   render()  {
     return (
       <div className="App">
-        <h1 className= 'error'>Error</h1>
-        <h1 className= {styles.success}>Success</h1>
+        <Form></Form>
+        {/* <h1 className= 'error'>Error</h1> */}
+        {/* <h1 className= {styles.success}>Success</h1> */}
         {/* <Inline /> */}
         {/* <Stylesheet primary = {true} /> */}
         {/* <NameList /> */}
---
+++ src/Form.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+class Form extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Form component
+            </div>
+        )
+    }
+}
+
+export default Form
