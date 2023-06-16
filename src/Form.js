d61adb0702601f27fa23664f4a96020477ae413b
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-16 04:27:51 +0100

    React js Form 'target.event.value'

--- src/Form.js
+++ src/Form.js
@@ -5,17 +5,31 @@ class Form extends PureComponent {
         super(props)
 
         this.state = {
+            username: ''
             
         }
     }
+        handleUsernameChange = (event) => {
+            this.setState({
+                username: event.target.value
+            })
+        }
+        
+    
 
     render() {
         return (
+            <form>
             <div>
-                Form component
+                <label>Username</label>
+                <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
             </div>
+
+            </form>
         )
     }
-}
+
+    }
+
 
 export default Form
