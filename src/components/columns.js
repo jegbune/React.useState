61a84699306b5f6813c62865f8ab99a68bfed107
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-25 12:31:55 +0100

    React.Fragment item.map()

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Columns() {
+    const items = []
     return (
-        <div>
+        <React.Fragment>
+            {
+                items.map( item => (
+                    <React.Fragment key={item.id}>
+                        <h1>Title</h1>
+                        <p>{item.title}</p>
+                    </React.Fragment>    
+                ))
+            }
             <td>Name</td>
             <td>Jeremiah</td>
-        </div>
+        </React.Fragment>
     )
 }
 
