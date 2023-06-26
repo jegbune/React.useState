db54b5eccafb8268b1ca0ae1af029f31dbb7efd6
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-06-26 12:34:27 +0100

    React snippet for React.Fragment; Shortcut

--- src/components/columns.js
+++ src/components/columns.js
@@ -3,18 +3,10 @@ import React from 'react'
 function Columns() {
     const items = []
     return (
-        <React.Fragment>
-            {
-                items.map( item => (
-                    <React.Fragment key={item.id}>
-                        <h1>Title</h1>
-                        <p>{item.title}</p>
-                    </React.Fragment>    
-                ))
-            }
+        <>
             <td>Name</td>
             <td>Jeremiah</td>
-        </React.Fragment>
+        </>
     )
 }
 
