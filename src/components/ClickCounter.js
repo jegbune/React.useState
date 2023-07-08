12e0df76687b3996707f38f1e526ec0e8c7be712
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-08 14:39:07 +0100

    React Lesson 2 on Higer Order

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,16 +1,17 @@
 import React, { PureComponent } from 'react'
-import UpdatedComponent from '../withCounter'
+import withCounter from '../withCounter'
 
 class ClickCounter extends PureComponent {
     
-    render() {
+    render() { 
         const { count, incrementCount} = this.props
         return (
             <div> 
-                <button onClick={incrementCount}> Clicked {count} times</button>
+                <button onClick={incrementCount}> 
+                {this.props.name} Clicked {count} times</button>
             </div>
         )
     }
 }
 
-export default UpdatedComponent(ClickCounter)
+export default withCounter(ClickCounter)
