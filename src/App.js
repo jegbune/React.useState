ba2e0a19bab7de7b75efb27feda5368c07df217a
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-11 14:59:05 +0100

    React lesson on Render Props in share commonw Functionality

--- src/App.js
+++ src/App.js
@@ -27,13 +27,20 @@ import Hero from "./components/Hero"
 import ErrorBoundary from "./components/ErrorBoundary";
 import ClickCounter from "./components/ClickCounter";
 import HoverCounter from "./components/HoverCounter";
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import HoverCounterTwo from "./components/HoverCounterTwo";
 class App extends Component {
   render()  {
     return (
       <div className="App">
-        <ClickCounter name ='Jeremiah' />
 
-        <HoverCounter name = 'Jeremiah' />.
+      <ClickCounterTwo />
+
+      <HoverCounterTwo />
+
+        {/* <ClickCounter name ='Jeremiah' /> */}
+
+        {/* <HoverCounter name = 'Jeremiah' />. */}
 
         {/* <ErrorBoundary>
         <Hero heroName='Batman'/>
