6108f54dbba54024b6207d8ec4f1e7d0be811204
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-20 00:11:49 +0100

    React lesson on Form and onChange attribute

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render()  {
     return (
       <div className="App">
-        <Form></Form>
+        <Form />
         {/* <h1 className= 'error'>Error</h1> */}
         {/* <h1 className= {styles.success}>Success</h1> */}
         {/* <Inline /> */}
